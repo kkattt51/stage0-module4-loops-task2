95b9d600580c871107c6b94ced78ad4e3261a99d
Kislitskaya_I <IKislitskaya@ibagroup.eu>
2022-07-18 17:14:01 +0300

    task 2 loops module 4 stage0

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int number = 0;
+        int fact = 1;
+        while (number <= printToInclusive) {
+            if (number == 0) {
+                System.out.println(fact);
+            } else {
+                System.out.println(fact *= number);
+            }
+            number++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int number = 0;
+        if (power < 0) {
+            System.out.println("too much power");
+        } else {
+            while (number <= power) {
+                System.out.println((int) Math.pow(2, number));
+                number++;
+            }
+        }
     }
 }
